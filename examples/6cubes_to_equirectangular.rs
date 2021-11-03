512694a2d0eaec58bc45be2d81af7aa7440c7bfa
DESKTOP-RFPJ7SV\earne <earnest.marshi@gmail.com>
2021-11-03 16:41:40 +0900

    panorama circle

--- examples/6cubes_to_equirectangular.rs
+++ examples/6cubes_to_equirectangular.rs
@@ -72,8 +72,6 @@ fn main() {
         in vec2 position;
         out vec2 v_tex_coords;
 
-        uniform mat4 matrix;
-
         void main() {
             v_tex_coords = position;
             gl_Position = vec4(position, 0.0, 1.0);
