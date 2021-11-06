2df61807e92783b9b0cad53a44387dbc5f7ff83a
DESKTOP-RFPJ7SV\earne <earnest.marshi@gmail.com>
2021-11-06 16:54:45 +0900

    fix for electron

--- src/pano.js
+++ src/pano.js
@@ -1,2 +1,9 @@
 import init from "../pano-rs/pkg/pano.js";
-init();
+import * as wasm from '../pano-rs/pkg/pano.js';
+
+// https://github.com/anderejd/electron-wasm-rust-example
+async function run() {
+	await init('../pano-rs/pkg/pano_bg.wasm');
+    console.log(wasm);
+}
+run();
