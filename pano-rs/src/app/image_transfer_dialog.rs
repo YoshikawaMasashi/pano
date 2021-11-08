d5043f1f91890cf85b82c60135a95a030593ab2c
DESKTOP-RFPJ7SV\earne <earnest.marshi@gmail.com>
2021-11-09 00:01:58 +0900

    image transfer

--- pano-rs/src/app/image_transfer_dialog.rs
+++ pano-rs/src/app/image_transfer_dialog.rs
@@ -12,7 +12,11 @@ use crate::webgl_utils::{compile_shader, get_uniform_locations, link_program};
 const WORK_TEXTURE_WIDTH: usize = 3840;
 const WORK_TEXTURE_HEIGHT: usize = 1920;
 
-pub enum Msg {}
+pub enum Msg {
+    OpenInputImageDialog,
+    OpenOutputImageDialog,
+    ExecuteTransfer,
+}
 
 #[derive(Properties, Clone, PartialEq)]
 pub struct Props {
@@ -23,32 +27,129 @@ pub struct ImageTransferDialog {
     #[allow(dead_code)]
     link: ComponentLink<Self>,
     webgl: Option<Arc<RwLock<ModelWebGL>>>,
+    input_of_input_image_ref: NodeRef,
+    input_of_output_image_ref: NodeRef,
 
     open: bool,
 }
 
-pub struct ModelWebGL {}
+pub struct ModelWebGL {
+    context: WebGl2RenderingContext,
+    image_transfer_vert_shader: WebGlShader,
+    image_transfer_frag_shader: WebGlShader,
+}
 
 impl Component for ImageTransferDialog {
     type Message = Msg;
     type Properties = Props;
 
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let input_of_input_image_ref = NodeRef::default();
+        let input_of_output_image_ref = NodeRef::default();
         Self {
             link,
             webgl: None,
+            input_of_input_image_ref,
+            input_of_output_image_ref,
 
             open: props.open,
         }
     }
 
     fn rendered(&mut self, first_render: bool) {
-        if first_render {}
+        if first_render {
+            let document = web_sys::window().unwrap().document().unwrap();
+            let canvas = document.get_element_by_id("6cubes_canvas").unwrap();
+            let canvas: web_sys::HtmlCanvasElement =
+                canvas.dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+
+            let context = canvas
+                .get_context("webgl2")
+                .unwrap()
+                .unwrap()
+                .dyn_into::<WebGl2RenderingContext>()
+                .unwrap();
+            context.clear_color(0.0, 0.0, 0.0, 1.0);
+            context.enable(WebGl2RenderingContext::BLEND);
+
+            let image_transfer_vert_shader = compile_shader(
+                &context,
+                WebGl2RenderingContext::VERTEX_SHADER,
+                include_str!("../shaders/image_transfer.vert"),
+            )
+            .unwrap();
+            let image_transfer_frag_shader = compile_shader(
+                &context,
+                WebGl2RenderingContext::FRAGMENT_SHADER,
+                include_str!("../shaders/image_transfer.frag"),
+            )
+            .unwrap();
+
+            self.webgl = Some(Arc::new(RwLock::new(ModelWebGL {
+                context,
+                image_transfer_vert_shader,
+                image_transfer_frag_shader,
+            })));
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {}
-        false
+        match msg {
+            Msg::OpenInputImageDialog => {
+                let dialog_promise: js_sys::Promise =
+                    crate::wasm_bind::show_open_png_dialog().unwrap().into();
+                let input_of_input_image_ref = self.input_of_input_image_ref.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let path_or_undefined = wasm_bindgen_futures::JsFuture::from(dialog_promise)
+                        .await
+                        .unwrap();
+                    if let Some(path) = path_or_undefined.as_string() {
+                        if let Some(input) = input_of_input_image_ref.cast::<HtmlInputElement>() {
+                            input.set_value(path.as_str());
+                        }
+                    }
+                });
+                false
+            }
+            Msg::OpenOutputImageDialog => {
+                let dialog_promise: js_sys::Promise =
+                    crate::wasm_bind::show_save_png_dialog().unwrap().into();
+                let input_of_output_image_ref = self.input_of_output_image_ref.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let path_or_undefined = wasm_bindgen_futures::JsFuture::from(dialog_promise)
+                        .await
+                        .unwrap();
+                    if let Some(path) = path_or_undefined.as_string() {
+                        if let Some(input) = input_of_output_image_ref.cast::<HtmlInputElement>() {
+                            input.set_value(path.as_str());
+                        }
+                    }
+                });
+                false
+            }
+            Msg::ExecuteTransfer => {
+                if let Some(input_of_input_image) =
+                    self.input_of_input_image_ref.cast::<HtmlInputElement>()
+                {
+                    if let Some(input_of_output_image) =
+                        self.input_of_output_image_ref.cast::<HtmlInputElement>()
+                    {
+                        let input_image_path = input_of_input_image.value();
+                        let input_image_path = Path::new(&input_image_path);
+                        let output_image_path = input_of_output_image.value();
+                        let output_image_path = Path::new(&output_image_path);
+                        self.webgl
+                            .as_ref()
+                            .unwrap()
+                            .read()
+                            .unwrap()
+                            .transfer(input_image_path, output_image_path)
+                            .unwrap();
+                    }
+                }
+                false
+            }
+        }
     }
 
     fn change(&mut self, props: Self::Properties) -> ShouldRender {
@@ -68,8 +169,127 @@ impl Component for ImageTransferDialog {
                     open=self.open
                 >
                     {"Image Transfer"}
+                    <br />
+                    {"Input Image"}
+                    <br />
+                    <input
+                        ref={self.input_of_input_image_ref.clone()}
+                    />
+                    <button onclick=self.link.callback(|_| Msg::OpenInputImageDialog)>{ "ファイルを選択" }</button>
+                    <br />
+                    <br />
+                    {"Output Image"}
+                    <br />
+                    <input
+                        ref={self.input_of_output_image_ref.clone()}
+                    />
+                    <button onclick=self.link.callback(|_| Msg::OpenOutputImageDialog)>{ "ファイルを選択" }</button>
+                    <br />
+                    <button onclick=self.link.callback(|_| Msg::ExecuteTransfer)>{ "実行" }</button>
                 </dialog>
             </div>
         }
+    }
+}
+
+impl ModelWebGL {
+    pub fn transfer(
+        &self,
+        input_image_path: &Path,
+        output_image_path: &Path,
+    ) -> Result<(), JsValue> {
+        let input_image = read_image(input_image_path);
+
+        let input_texture = self.context.create_texture().unwrap();
+        let output_texture = self.context.create_texture().unwrap();
+
+        for (texture, pixels) in [
+            (&input_texture, Some(input_image.as_raw().as_slice())),
+            (&output_texture, None),
+        ] {
+            self.context
+                .bind_texture(WebGl2RenderingContext::TEXTURE_2D, Some(&texture));
+            self.context
+                .tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
+                    WebGl2RenderingContext::TEXTURE_2D,
+                    0,
+                    WebGl2RenderingContext::RGBA as i32,
+                    WORK_TEXTURE_WIDTH as i32,
+                    WORK_TEXTURE_HEIGHT as i32,
+                    0,
+                    WebGl2RenderingContext::RGBA,
+                    WebGl2RenderingContext::UNSIGNED_BYTE,
+                    pixels,
+                )?;
+            self.context.tex_parameteri(
+                WebGl2RenderingContext::TEXTURE_2D,
+                WebGl2RenderingContext::TEXTURE_MAG_FILTER,
+                WebGl2RenderingContext::LINEAR as i32,
+            );
+            self.context.tex_parameteri(
+                WebGl2RenderingContext::TEXTURE_2D,
+                WebGl2RenderingContext::TEXTURE_MIN_FILTER,
+                WebGl2RenderingContext::LINEAR as i32,
+            );
+            self.context
+                .bind_texture(WebGl2RenderingContext::TEXTURE_2D, None);
+        }
+
+        let frame_buffer = self.context.create_framebuffer().unwrap();
+        self.context
+            .bind_framebuffer(WebGl2RenderingContext::FRAMEBUFFER, Some(&frame_buffer));
+
+        self.context.framebuffer_texture_2d(
+            WebGl2RenderingContext::FRAMEBUFFER,
+            WebGl2RenderingContext::COLOR_ATTACHMENT0,
+            WebGl2RenderingContext::TEXTURE_2D,
+            Some(&output_texture),
+            0,
+        );
+        self.context
+            .viewport(0, 0, WORK_TEXTURE_WIDTH as i32, WORK_TEXTURE_HEIGHT as i32);
+
+        let program = link_program(
+            &self.context,
+            &self.image_transfer_vert_shader,
+            &self.image_transfer_frag_shader,
+        )?;
+        let uniforms = get_uniform_locations(&self.context, &program, vec!["tex".to_string()])?;
+        self.context.use_program(Some(&program));
+
+        self.context
+            .active_texture(WebGl2RenderingContext::TEXTURE0);
+        self.context
+            .bind_texture(WebGl2RenderingContext::TEXTURE_2D, Some(&input_texture));
+        self.context.uniform1i(Some(&uniforms["tex"]), 0);
+
+        self.context.blend_func(
+            WebGl2RenderingContext::SRC_ALPHA,
+            WebGl2RenderingContext::ONE_MINUS_SRC_ALPHA,
+        );
+        self.context
+            .draw_arrays(WebGl2RenderingContext::TRIANGLES, 0, 6);
+
+        let mut data: Vec<u8> = vec![0; WORK_TEXTURE_WIDTH * WORK_TEXTURE_HEIGHT * 4];
+        self.context.read_pixels_with_opt_u8_array(
+            0,
+            0,
+            WORK_TEXTURE_WIDTH as i32,
+            WORK_TEXTURE_HEIGHT as i32,
+            WebGl2RenderingContext::RGBA,
+            WebGl2RenderingContext::UNSIGNED_BYTE,
+            Some(data.as_mut_slice()),
+        )?;
+
+        self.context
+            .bind_texture(WebGl2RenderingContext::TEXTURE_2D, None);
+        self.context
+            .bind_framebuffer(WebGl2RenderingContext::FRAMEBUFFER, None);
+
+        let data =
+            image::RgbaImage::from_vec(WORK_TEXTURE_WIDTH as u32, WORK_TEXTURE_HEIGHT as u32, data)
+                .unwrap();
+        write_image(output_image_path, data);
+        Ok(())
     }
 }
---
+++ pano-rs/src/shaders/image_transfer.frag
@@ -0,0 +1,119 @@
+#version 300 es
+#define PI 3.1415926535897932384626
+#define X 0.525731112119133606
+#define Z 0.850650808352039932
+#define N 0.0
+
+precision highp float;
+
+in vec2 fragment_position;
+out vec4 color;
+
+uniform sampler2D tex;
+
+const vec3[12] ICO_VERTICES = vec3[](
+    vec3(-X, N, Z),
+    vec3(X, N, Z),
+    vec3(-X, N, -Z),
+    vec3(X, N, -Z),
+    vec3(N, Z, X),
+    vec3(N, Z, -X),
+    vec3(N, -Z, X),
+    vec3(N, -Z, -X),
+    vec3(Z, X, N),
+    vec3(-Z, X, N),
+    vec3(Z, -X, N),
+    vec3(-Z, -X, N)
+);
+const int[60] ICO_INDICES = int[](
+    0, 4, 1,
+    0, 9, 4,
+    9, 5, 4,
+    4, 5, 8,
+    4, 8, 1,
+    8, 10, 1,
+    8, 3, 10,
+    5, 3, 8,
+    5, 2, 3,
+    2, 7, 3,
+    7, 10, 3,
+    7, 6, 10,
+    7, 11, 6,
+    11, 0, 6,
+    0, 1, 6,
+    6, 1, 10,
+    9, 0, 11,
+    9, 11, 2,
+    9, 2, 5,
+    7, 2, 11
+);
+
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+void main() {
+    int res = 180;
+
+    color = vec4(0.0, 0.0, 0.0, 0.0);
+
+    for (int triangle = 0; triangle < 20; triangle++){
+        for(int i = 0; i <= res; i++) {
+            for(int j = 0; j <= res - i; j++) {
+                int k = res - i - j;
+                vec3 ratio = vec3(float(i) / float(res), float(j) / float(res), float(k) / float(res));
+                vec3 v1 = vec3(ICO_VERTICES[ICO_INDICES[triangle * 3]]);
+                vec3 v2 = vec3(ICO_VERTICES[ICO_INDICES[triangle * 3 + 1]]);
+                vec3 v3 = vec3(ICO_VERTICES[ICO_INDICES[triangle * 3 + 2]]);
+                vec3 v = v1 * ratio.x + v2 * ratio.y + v3 * ratio.z;
+                v.x += 0.003 * (2.0 * rand(v.yz) - 1.0);
+                v.y += 0.003 * (2.0 * rand(v.zx) - 1.0);
+                v.z += 0.003 * (2.0 * rand(v.xy) - 1.0);
+                v = normalize(v);
+                
+                float v_elevation = asin(v.y);
+                float v_azimuth = sign(v.x) * acos(v.z / cos(v_elevation));
+                vec4 v_color = texture(tex, vec2(0.5 * v_azimuth / PI + 0.5, 0.5 * v_elevation / PI * 2.0 + 0.5));
+
+                vec3 position = vec3(-v_elevation * 180.0 / PI, -v_azimuth * 180.0 / PI, 0.0);
+                // TODO: Color Random
+
+                float scale = 0.005 + 0.005 * rand(vec2(v_azimuth + float(i), v_elevation + float(j)));
+                
+                float azimuth = fragment_position.x * PI;
+                float elevation = fragment_position.y * PI / 2.0;
+
+                vec3 pt;
+                pt.x = cos(elevation) * sin(azimuth);
+                pt.y = sin(elevation);
+                pt.z = cos(elevation) * cos(azimuth);
+
+                vec3 rotation_eular = -position / 180.0 * PI;
+                mat3 rotation_x = mat3(
+                    vec3(1, 0.0, 0.0),
+                    vec3(0.0, cos(rotation_eular.x), -sin(rotation_eular.x)),
+                    vec3(0.0, sin(rotation_eular.x), cos(rotation_eular.x))
+                );
+                mat3 rotation_y = mat3(
+                    vec3(cos(rotation_eular.y), 0.0, sin(rotation_eular.y)),
+                    vec3(0.0, 1.0, 0.0),
+                    vec3(-sin(rotation_eular.y), 0.0, cos(rotation_eular.y))
+                );
+                mat3 rotation_z = mat3(
+                    vec3(cos(rotation_eular.z), -sin(rotation_eular.z), 0.0),
+                    vec3(sin(rotation_eular.z), cos(rotation_eular.z), 0.0),
+                    vec3(0.0, 0.0, 1.0)
+                );
+                mat3 rotation = rotation_x * rotation_y * rotation_z;
+                pt = rotation * pt;
+
+                if (pt.z >= 0.0) {
+                    vec2 plane_pos = vec2(pt.x / pt.z, pt.y / pt.z);
+                    if (sqrt(plane_pos.x * plane_pos.x + plane_pos.y * plane_pos.y) <= scale) {
+                        color = v_color;
+                    }
+                }
+            }
+        }
+    }
+}
